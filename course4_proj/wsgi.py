4fa235bf37aa68c44ef9de2d5ef41e6f8f5fd943
Henry Diaz <henrydcmaster@gmail.com>
2025-04-03 13:26:50 -0600

    Finish omdb api

--- course4_proj/wsgi.py
+++ course4_proj/wsgi.py
@@ -1,17 +1,16 @@
 """
-WSGI config for blango project.
+WSGI config for course4_proj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "course4_proj.settings")
-os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
+from django.core.wsgi import get_wsgi_application
 
-from configurations.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "course4_proj.settings")
 
 application = get_wsgi_application()
